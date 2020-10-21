b2e254d2b9584d92cce8625de222deba6138382f
bkkater <biancamedeiros.bs10@gmail.com>
2020-10-21 20:48:15 -0300

    [add] home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//Componentes
+import Header from '../../components/Header';
+
+//Styles
+import './styles.css';
+
+const Home: React.FC = () => {
+    return (
+        <div id="page-home">
+            <Header/>
+        </div>
+    );
+}
+
+export default Home;
