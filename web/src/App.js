8fc8e5466cf6e0bd22da2054c47fa9c759497b84
elton de campos <eltoncampos36@gmail.com>
2021-06-01 23:32:19 -0300

    chore: add global styles in App component

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,13 @@
 import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+    </>
+  );
 }
 
 export default App;
