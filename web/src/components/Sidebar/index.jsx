f7036041bbec7a4417420fc98282207c4f6e0aff
elton de campos <eltoncampos36@gmail.com>
2021-06-02 00:44:19 -0300

    chore: create sidebar component

---
+++ web/src/components/Sidebar/index.jsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+export function SideBar() {
+    return (
+        <S.Container>
+            <h1>sidebar</h1>
+        </S.Container>
+    )
+}
---
+++ web/src/components/Sidebar/styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Container = styled.aside`
+  grid-area: sidebar;
+  background: #ccc;
+`;
