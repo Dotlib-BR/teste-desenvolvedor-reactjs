dd07a5131dc19b0d0e036d398a332071ca45afeb
elton de campos <eltoncampos36@gmail.com>
2021-06-02 00:45:11 -0300

    chore: render home component in App file

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,11 @@
-import { Header } from "./components/Header";
+import { Home } from "./pages/Home";
 import { GlobalStyle } from "./styles/global";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Home />
     </>
   );
 }
