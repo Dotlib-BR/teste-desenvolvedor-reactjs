e15a3b43e5d4e8e90b6810853bda91a68b2cc3a2
elton de campos <eltoncampos36@gmail.com>
2021-06-03 21:25:17 -0300

    feat: create a sideBar

--- web/src/components/Sidebar/index.jsx
+++ web/src/components/Sidebar/index.jsx
@@ -1,9 +1,27 @@
-import * as S from './styles'
+import { useEffect, useState } from 'react';
+import api from '../../utils/api'
+
+import * as S from './styles';
 
 export function SideBar() {
+    const [category, setCategory] = useState([]);
+
+
+    useEffect(() => {
+        api.get('/categories').then((category) => {
+            setCategory(category.data)
+        })
+    },[])
+
     return (
         <S.Container>
-            <h1>sidebar</h1>
+            <h1>Assunto</h1>
+                {category.map((category) => (
+                    <S.CheckBoxContainer key={category.id}>
+                        <input type="checkbox"  />
+                        <label >{category.title}</label>
+                    </S.CheckBoxContainer>
+                ))}
         </S.Container>
     )
 }
